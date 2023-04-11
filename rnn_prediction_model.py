ae6e9cc18086721a270b8a3e49f4d57f3bd40708
Mikhail Larin <larin.mikhail.84@mai.ru>
2023-04-11 13:14:53 +0700

    '1st'

---
+++ rnn_prediction_model.py
@@ -0,0 +1,74 @@
+import keras.optimizers
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.optimizers import Adam
+
+
+# Training dataset
+dataset_train = pd.read_csv('stock_price_train.csv')
+training_set = dataset_train.iloc[:, 1:2].values
+
+# Feature Scaling
+sc = MinMaxScaler(feature_range=(0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+
+X_train = []
+y_train = []
+for i in range(60, 1258):
+    X_train.append(training_set_scaled[i-60:i, 0])
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+# Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+# Initialising the model
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+# Compiling the model
+model.compile(optimizer=keras.optimizers.Adam(0.1), loss='mean_squared_error')
+
+# Fitting the model
+model.fit(X_train, y_train, epochs=100, batch_size=32)
+
+# Getting the real stock price of 2017
+dataset_test = pd.read_csv('stock_price_test.csv')
+real_stock_price = dataset_test.iloc[:, 1:2].values
+
+# Getting the predicted stock price of 2017
+dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis=0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 80):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = model.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# Visualising the results
+plt.plot(real_stock_price, color='red', label='Real Stock Price')
+plt.plot(predicted_stock_price, color='blue', label='Predicted Stock Price')
+plt.title('Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
